ecce25fcb30f76abedaec123a3b3464718aa9d58
Sithum Raveesha <85311920+sithum20210380@users.noreply.github.com>
2022-09-20 14:19:53 +0530

    Add files via upload

---
+++ lib/home/big_text.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/cupertino.dart';
+
+class BigText extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size;
+  TextOverflow overFlow;
+  BigText(
+      {Key? key,
+      this.color = const Color(0xFF332d2d),
+      required this.text,
+      this.size = 20,
+      this.overFlow = TextOverflow.ellipsis})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      maxLines: 1,
+      overflow: overFlow,
+      style: TextStyle(
+          fontFamily: 'Roboto',
+          color: color,
+          fontSize: size,
+          fontWeight: FontWeight.w700),
+    );
+  }
+}
